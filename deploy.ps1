8860911cf3148ebcd6e21c9892fe2c7600cf9748
kalyansuresh <kalyansuresh743@gmail.com>
2023-08-01 16:02:22 +0530

    upadted ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,17 +1,27 @@
 param (
     [string]$ResourceGroupName,
     [string]$FunctionAppName,
-    [string]$Location
+    [string]$Location,
+    [string]$ClientId,
+    [string]$ClientSecret,
+    [string]$TenantId
 )
 
-# Connect to Azure
-Connect-AzAccount
+# Install Azure PowerShell module if not already installed
+if (-not (Get-Module -ListAvailable Az)) {
+    Install-Module -Name Az -Force -AllowClobber -Scope CurrentUser
+}
+
+# Connect to Azure using service principal credentials
+$secureClientSecret = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
+$psCredential = New-Object System.Management.Automation.PSCredential ($ClientId, $secureClientSecret)
+Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCredential
 
 # Create Resource Group
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 
 # Create Function App
-New-AzFunctionApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName -StorageAccount $FunctionAppName -Runtime 'dotnet'
+New-AzFunctionApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName -Location $Location -Runtime 'dotnet'
 
 # You can add more configuration and settings for your Function App if needed.
 
