5d4594be98d038928645ae31c180e0c3ee579720
kalyansuresh <kalyansuresh743@gmail.com>
2023-08-01 17:12:03 +0530

    upadted ps1-4

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,8 @@ param (
     [string]$Location,
     [string]$ClientId,
     [string]$ClientSecret,
-    [string]$TenantId
+    [string]$TenantId,
+    [string]$SubscriptionId
 )
 
 # Install Azure PowerShell module if not already installed
@@ -13,12 +14,14 @@ if (-not (Get-Module -ListAvailable Az)) {
 }
 
 # Set the Azure subscription context
-Set-AzContext -SubscriptionId "3718112c-afe8-4000-8156-64d160081570" -TenantId "d5b66d53-29b2-4452-ba0f-f8badcf5782d"
-
-# Connect to Azure using service principal credentials
 $secureClientSecret = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
 $psCredential = New-Object System.Management.Automation.PSCredential ($ClientId, $secureClientSecret)
-Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCredential
+$azConnection = Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCredential -ErrorAction SilentlyContinue
+
+if (!$azConnection) {
+    # If the connection attempt failed, try again with the subscription ID
+    Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCredential -SubscriptionId $SubscriptionId
+}
 
 # Create Resource Group
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location
