cc789e7a85faabc88701cf2eb9e0f06eabbbc30d
kalyansuresh <kalyansuresh743@gmail.com>
2023-08-02 10:22:11 +0530

    one more try

--- deploy.ps1
+++ deploy.ps1
@@ -16,11 +16,11 @@ if (-not (Get-Module -ListAvailable Az)) {
 # Set the Azure subscription context
 $secureClientSecret = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
 $psCredential = New-Object System.Management.Automation.PSCredential ($ClientId, $secureClientSecret)
-$azConnection = Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCredential -ErrorAction SilentlyContinue
+$azConnection = Connect-AzAccount -ServicePrincipal -Tenant d5b66d53-29b2-4452-ba0f-f8badcf5782d -Credential $psCredential -ErrorAction SilentlyContinue
 
 if (!$azConnection) {
     # If the connection attempt failed, try again with the subscription ID
-    Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $psCredential -SubscriptionId 3718112c-afe8-4000-8156-64d160081570
+    Connect-AzAccount -ServicePrincipal -Tenant d5b66d53-29b2-4452-ba0f-f8badcf5782d -Credential $psCredential -SubscriptionId 3718112c-afe8-4000-8156-64d160081570
 }
 
 # Create Resource Group
